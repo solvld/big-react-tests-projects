6ed8eacb1d07ba5d4494e7ce7e77d97da0e634f8
solvld <sol.vld@yandex.ru>
2023-11-15 22:59:37 +0400

    add example fetch hook project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import ForumApp from './Projects/Forum/ForumApp';
 import GroceryApp from './Projects/Grocery/GroceryApp';
+import ExampleFetch from './Projects/ExampleFetch/ExampleFetch'
 
 function App() {
   return (
-    <div className="App">
-      {/* <ForumApp /> */}
+    <div className='App'>
+      <ForumApp />
       <hr/>
       <GroceryApp />
+      <hr/> 
+      <ExampleFetch />
     </div>
   );
 }
