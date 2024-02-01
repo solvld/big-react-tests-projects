e5971dbcca1b0ce435451aab822459bcc04713ee
solvld <sol.vld@yandex.ru>
2024-02-01 13:01:31 +0400

    add Theme Toggler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
 // import ForumApp from './Projects/Forum/ForumApp';
 import GroceryApp from './Projects/Grocery/GroceryApp';
 //import ExampleFetch from './Projects/ExampleFetch/ExampleFetch';
 import Showcase from './Projects/RandomFontAwesomButton/RandomButton';
 import PassingThoughtsApp from './Projects/PassingThoughts/PassingThoughtsApp';
-
+import Header from './Projects/themeToggler/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       {/* <ForumApp /> */}
       <PassingThoughtsApp />
       <hr />
---
+++ src/Projects/themeToggler/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTheme } from '../../hooks/useTheme';
+import styles from './styles.module.css'
+
+const Header = () => {
+  const [onToggleTheme, isDarkTheme] = useTheme();
+
+  return (
+    <header className={styles.header}>
+      <h2>Change Theme</h2>
+      <section className="actionsContainer">
+        <button onClick={onToggleTheme}>
+          {isDarkTheme ? 'Change to 🌞 mode' : 'Change to 🌚 mode'}
+        </button>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
