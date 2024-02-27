3e743af490277102fa213b2bbe02a6b7829ae992
solvld <sol.vld@yandex.ru>
2024-02-27 11:52:47 +0400

    bug fix and edit styles

--- src/Projects/ShopStore/app/index.jsx
+++ src/Projects/ShopStore/app/index.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store.js';
 
 import { Inventory } from '../features/inventory/Inventory.jsx';
 import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.jsx';
 import { Cart } from '../features/cart/Cart.jsx';
+import { store } from './store.js';
+
+import '../style.css';
 
 const ShopStoreApp = () => {
-  const {state,dispatch } = {}
+  const state = store.getState();
+  const dispatch = store.dispatch;
 
   return (
-    <Provider store={store}>
-      <div>
-        <CurrencyFilter
-          currencyFilter={state.currencyFilter}
-          dispatch={dispatch}
-        />
+    <div id="app">
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
 
-        <Inventory
-          inventory={state.inventory}
-          currencyFilter={state.currencyFilter}
-          dispatch={dispatch}
-        />
-        <Cart
-          cart={state.cart}
-          currencyFilter={state.currencyFilter}
-          dispatch={dispatch}
-        />
-      </div>
-    </Provider>
+      <Inventory
+        inventory={state.inventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+      <Cart
+        cart={state.cart}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import { Provider } from 'react-redux';
+import { store } from './Projects/ShopStore/app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const render = () => {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
+};
+
+render();
+
+store.subscribe(render);
